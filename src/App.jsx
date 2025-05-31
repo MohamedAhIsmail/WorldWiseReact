30d955ea5d7b7479641b150fc3fbdeb2e7ce4879
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-05-31 10:57:10 +0300

    Created nested routes for app layout component

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />}/>
-        <Route path='login' element={<Login />}></Route>
-        <Route path="*" element={<PageNotFound/>} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities🤴</p>}/>
+          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
