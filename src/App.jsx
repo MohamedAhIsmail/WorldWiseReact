3dca9d279948b2414b42d71ef3b2e1509a04f8e9
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-05-31 09:37:34 +0300

    Main Pages Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import NotFoundPage from "./pages/NotFoundPage";
 import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}/>
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path='login' element={<Login />}></Route>
+        <Route path="*" element={<PageNotFound/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,25 @@
-import AppNav from "../components/AppNav";
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
 import PageNav from "../components/PageNav";
 
-function HomePage() {
+export default function Homepage() {
   return (
-    <>
-      <PageNav />
-      <AppNav />
-      <h1>World Wise</h1>
-    </>
+    <main className={styles.homepage}>
+        <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+
+        <Link to="/app" className="cta">Start tracking now</Link>
+      </section>
+    </main>
   );
 }
-
-export default HomePage;
