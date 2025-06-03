978d21ee3f1cfea2ddf37ebaaab9f9a030e0f0ae
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-03 10:36:29 +0300

    useParams and useSearchParams to get data in components

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -6,7 +6,12 @@ import Spinner from "./Spinner";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  if (!cities.length) return <Message message={"Add your first city by clicking on a city on the map"} />;
+  if (!cities.length)
+    return (
+      <Message
+        message={"Add your first city by clicking on a city on the map"}
+      />
+    );
 
   return (
     <div>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,16 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return <div className={styles.mapContainer}>
+    <h1>Map</h1>
+    <h1>Possition: {lat} , {lng}</h1>
+    
+  </div>;
 }
 
 export default Map;
