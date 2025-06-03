e656135ccff69a5bda9ff9740f3694e82d18f299
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-03 11:39:54 +0300

    Created City Form and useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -44,18 +45,25 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={
+              <Navigate
+                to="cities"
+                replace
+                cities={cities}
+                isLoading={isLoading}
+              />
+            }
           />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="cities/:id" element={<City />}/>
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />} />
